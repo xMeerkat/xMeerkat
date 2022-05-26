e4c8cf70c697c192eeac01e274805e87d0a77796
gemsvido <gemsvido@gmail.com>
2022-05-26 03:02:37 -0700

    A hell ton of website work. http://xMeerkat.com

--- Website/alert.js
+++ Website/alert.js
@@ -1,27 +1,34 @@
 fetch(`https://raw.githubusercontent.com/xMeerkat/xMeerkat/master/Website/status.json?updated=${Date.now()}`).then(response => response.json()).then(async data => {
 
-                if (data.offline == true) {
-                    eval(await (await fetch("https://unpkg.com/sweetalert2")).text());
+    eval(await (await fetch("https://unpkg.com/sweetalert2")).text());
 
-                    if (swal) {
-                        swal.fire({
-                            title: "xMeerkat",
-                            html: "Please note that this site is NOT FOR KIDS.",
-                            icon: "info"
-                        });
-                    }
 
-                } else {
+    if (swal) {
 
-                    eval(await (await fetch("https://unpkg.com/sweetalert2")).text());
+        const Mix = Swal.mixin({
+            icon: "info",
+            showCancelButton: true,
+            toast: false,
+            confirmButtonText: "I am 18+",
+            cancelButtonText: "I am not 18+",
+        });
 
-                    if (swal) {
-                        swal.fire({
-                            title: "xMeerkat",
-                            html: "Please note that this site is NOT FOR KIDS.",
-                            icon: "info"
-                        });
-                    }
 
-                }
+        if (!(await Mix.fire("xMeerkat", "This website is not for anyone under the age of 18.")).value) {
+            window.open(window.location.href + "404", "");
+            await new Promise(r => setTimeout(r, 1000));
+            return window.close();
+        } else {
+
+            document.getElementById("MainImg").src = "https://raw.githubusercontent.com/x-Meerkat/main-image-xmeerkat/Image/img/assets/xm.jpeg";
+
+
+        }
+
+    }
+
+
 });
+
+
+// Minify this => Script in index.html
