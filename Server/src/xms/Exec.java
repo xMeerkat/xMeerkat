2bf41f55c08b5b010b425b4b9f3564b98f1ec00a
gemsvido <gemsvido@gmail.com>
2022-05-27 03:17:43 -0700

    GOOD NIGHT - http://xMeerkat.com

--- Server/src/xms/Exec.java
+++ Server/src/xms/Exec.java
@@ -2,8 +2,24 @@ package xms;
 
 public class Exec {
 
-    public static void main (String[] args) {
-        Main.main(args);
+    public static void main (String[] args) throws Exception {
+
+        Thread proc = new Thread(() -> {
+            Main.main(args);
+        });
+
+        proc.start();
+
+
+        while (true) {
+
+           if (!proc.isAlive()) {
+               proc.start();
+           }
+
+           Thread.sleep(10000);
+        }
+
     }
 
 
