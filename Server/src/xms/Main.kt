bad04fd070748a9008855df863d2e9fb92fe6599
gemsvido <gemsvido@gmail.com>
2022-06-06 03:42:31 -0700

    Fix crashing? Maybe idk - http://xMeerkat.com

--- Server/src/xms/Main.kt
+++ Server/src/xms/Main.kt
@@ -5,59 +5,43 @@
 
 package xms
 
-import xms.premium.pTokenStorage.xin as InitPremium
 import xms.runners.Runner443 as Port443
 import xms.runners.Runner80 as Port80
 
 
 object Main {
 
-    @JvmStatic private val runner : Thread = Thread {
 
-        println("\nRunning xMeerkat on http://localhost:80 and http://localhost:443\n")
+    @JvmStatic fun main (args: Array<String>) : Unit {s
 
-        val HTTP: Thread = Thread {
-            Port80.main()
+        while (true) {
+
+            try {
+
+                println("\nRunning xMeerkat on http://localhost:80 and http://localhost:443\n")
+
+                val HTTP: Thread = Thread {
+                    Port80.main()
+                }
+
+                val HTTPS: Thread = Thread {
+                    Port443.main()
+                }
+
+
+
+                MAPS.addMaps()
+
+                HTTP.start()
+                Port443.main()
+
+            } catch (xx : Exception) {
+                xx.printStackTrace()
+            }
+
+
         }
-
-        val HTTPS: Thread = Thread {
-            Port443.main()
-        }
-
-
-
-        MAPS.addMaps()
-
-        HTTP.start()
-        HTTPS.start()
     }
-
-
-    @JvmStatic fun main (args: Array<String>) : Unit {
-
-        val handler : Thread.UncaughtExceptionHandler = Thread.UncaughtExceptionHandler { _, e ->
-            e.printStackTrace()
-            println("Uncaught exception: $e")
-            runner.interrupt()
-            revive()
-        }
-
-
-        Thread.setDefaultUncaughtExceptionHandler(handler)
-        runner.uncaughtExceptionHandler = handler
-
-
-        InitPremium()
-
-
-        runner.start()
-    }
-
-    @JvmStatic fun revive () : Unit {
-
-        runner.run()
-    }
-
 
 
 }
